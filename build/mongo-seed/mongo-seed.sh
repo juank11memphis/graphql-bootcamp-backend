05ec2168fec40aab754f3b9084b0d40a62f4681e
Juan Carlos Morales Mora <juank.memphis@gmail.com>
2019-02-08 16:49:58 -0600

    mongo seed

--- build/mongo-seed/mongo-seed.sh
+++ build/mongo-seed/mongo-seed.sh
@@ -3,7 +3,7 @@
 
 echo 'seeding data into our database...'
 
-mongoimport --host mongodb --port 27017 --db movies --collection user-roles --type json --file data/user-roles.json --jsonArray
-mongoimport --host mongodb --port 27017 --db movies --collection users --type json --file data/users.json --jsonArray
+mongoimport --host mongodb --port 27017 --db movies --collection actors --type json --file data/actors.json --jsonArray
+mongoimport --host mongodb --port 27017 --db movies --collection movies --type json --file data/movies.json --jsonArray
 
 echo 'data seeding complete...'
