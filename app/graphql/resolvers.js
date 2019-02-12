67e4b71615f985b68549615d8dc39c8b96d3e218
Juan Carlos Morales Mora <juank.memphis@gmail.com>
2019-02-12 12:29:37 -0600

    branch05

--- app/graphql/resolvers.js
+++ app/graphql/resolvers.js
@@ -1,5 +1,10 @@
+import { MoviesService } from '../components'
+
 export default {
   Query: {
     getApiVersion: () => require('../../package.json').version,
+    getAllMovies: () => {
+      return MoviesService.getAllMovies()
+    },
   },
 }
--- app/graphql/schema.js
+++ app/graphql/schema.js
@@ -3,8 +3,25 @@ import { makeExecutableSchema } from 'graphql-tools'
 import resolvers from './resolvers'
 
 const typeDefs = `
+  type Actor {
+    id: String!
+    name: String
+  }
+
+  type Movie {
+    id: String!
+    genres: [String]
+    title: String
+    originalTitle: String
+    posterPath: String
+    overview: String
+    releaseDate: String
+    cast: [Actor]
+  }
+
   type Query {
     getApiVersion: String
+    getAllMovies: [Movie]
   }
 `
 
